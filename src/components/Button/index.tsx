bf1bdc8e5713fbae14ac1696ce929d23d842d3ba
kentaro19781123 <kens19781123@gmail.com>
2025-02-09 14:35:05 +0900

    tailwind custom class追加

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import "../../index.css";
 
 export const Button: React.FC = () => {
-  return <button className="text-3xl">Button</button>;
+  return <button className="text-3xl text-packages-color">Button</button>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "packages-color": "#f00",
+        "packages-color2": "#ff0",
+      },
+    },
   },
   plugins: [],
 };
