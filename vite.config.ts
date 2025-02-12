3a90e95a5cd2eb125e933d6a69d192b379fe5410
kentaro19781123 <kens19781123@gmail.com>
2025-02-12 23:43:16 +0900

    ファイル名変更

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss],
-    },
-  },
-  build: {
-    outDir: "dist", // default の設定と同じ
-    lib: {
-      entry: "src/index.ts",
-      name: "test-package",
-      // fileName: "index",
-      formats: ["es", "umd"], // default の設定と同じ
-      fileName: (format) => `test-package.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "tailwindcss"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          tailwindcss: "tailwindcss",
-        },
-      },
-    },
-  },
+	plugins: [react()],
+	css: {
+		postcss: {
+			plugins: [tailwindcss],
+		},
+	},
+	build: {
+		outDir: "dist", // default の設定と同じ
+		lib: {
+			entry: "src/index.ts",
+			name: "test-package",
+			// fileName: "index",
+			// formats: ["es", "umd", "cjs"], // default の設定と同じ
+			// fileName: (format) => `test-package.${format}.js`,
+		},
+		rollupOptions: {
+			external: ["react", "react-dom", "tailwindcss"],
+			output: {
+				globals: {
+					react: "React",
+					"react-dom": "ReactDOM",
+					tailwindcss: "tailwindcss",
+				},
+			},
+		},
+	},
 });
